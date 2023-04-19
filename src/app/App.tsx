9a082244f279b12114d12fbe2a730ba1a14a1dc7
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-04-19 12:28:01 -0700

    Add file structure from jsx version of app

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+import styles from "./App.module.css";
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+
+
+    </div>
+  )
+}
+
+export default App
