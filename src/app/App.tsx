c16afed34e8c318ca2d7fbcdffb961b57042bec8
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-04-19 19:32:06 -0700

    Add some nav formatting and styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,11 +8,32 @@ import styles from "./App.module.css";
 import Article from '../article/Article';
 import Layout from '../layout/Layout';
 import Modal from '../modal/Modal';
-import BusinessPage from '../pages/business/BusinessPage';
-import FoodPage from '../pages/food/FoodPage';
-import OpinionsPage from '../pages/opinion/OpinionsPage';
-import TopStoriesPage from '../pages/topstories/TopStoriesPage';
 import NoMatch from '../nomatch/NoMatch';
+import ArtsPage from '../pages/arts/ArtsPage';
+import AutomobilesPage from '../pages/automobiles/AutomobilesPage';
+import BooksPage from '../pages/books/BooksPage';
+import BusinessPage from '../pages/business/BusinessPage';
+import FashionPage from '../pages/fashion/FashionPage';
+import FoodPage from '../pages/food/FoodPage';
+import HealthPage from '../pages/health/HealthPage';
+import HomePage from '../pages/home/HomePage';
+import InsiderPage from '../pages/insider/InsiderPage';
+import MagazinePage from '../pages/magazine/MagazinePage';
+import MoviesPage from '../pages/movies/MoviesPage';
+import NYRegionPage from '../pages/nyregion/NYRegionPage';
+import ObituariesPage from '../pages/obituaries/ObituariesPage';
+import OpinionsPage from '../pages/opinion/OpinionsPage';
+import PoliticsPage from '../pages/politics/PoliticsPage';
+import RealEstatePage from '../pages/realestate/RealEstatePage';
+import SciencePage from '../pages/science/SciencePage';
+import SportsPage from '../pages/sports/SportsPage';
+import SundayReviewPage from '../pages/sundayreview/SundayReviewPage';
+import TechnologyPage from '../pages/technology/TechnologyPage';
+import TopStoriesPage from '../pages/topstories/TopStoriesPage';
+import TravelPage from '../pages/travel/TravelPage';
+import UpshotPage from '../pages/upshot/UpshotPage';
+import USPage from '../pages/us/USPage';
+import WorldPage from '../pages/world/WorldPage';
 
 
 export default function App() {
@@ -23,10 +44,31 @@ export default function App() {
     <div className={styles.app}>
       <Routes location={state?.backgroundLocation || location}>
         <Route path="/" element={<Layout/>}>
-          <Route path="/topstories" element={<TopStoriesPage />} />
-          <Route path="/opinion" element={<OpinionsPage />} />
+          <Route path="/arts" element={<ArtsPage/>} />
+          <Route path="/automobiles" element={<AutomobilesPage/>} />
+          <Route path="/books" element={<BooksPage/>} />
           <Route path="/business" element={<BusinessPage />} />
+          <Route path="/fashion" element={<FashionPage/>} />
           <Route path="/food" element={<FoodPage />} />
+          <Route path="/health" element={<HealthPage />} />
+          <Route index path="/home" element={<HomePage />} />
+          <Route path="/insider" element={<InsiderPage />} />
+          <Route path="/magazine" element={<MagazinePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/nyregion" element={<NYRegionPage />} />
+          <Route path="/obituaries" element={<ObituariesPage />} />
+          <Route path="/opinion" element={<OpinionsPage />} />
+          <Route path="/politics" element={<PoliticsPage />} />
+          <Route path="/realestate" element={<RealEstatePage />} />
+          <Route path="/science" element={<SciencePage />} />
+          <Route path="/sports" element={<SportsPage />} />
+          <Route path="/sundayreview" element={<SundayReviewPage />} />
+          <Route path="/technology" element={<TechnologyPage />} />
+          <Route path="/topstories" element={<TopStoriesPage />} />
+          <Route path="/travel" element={<TravelPage />} />
+          <Route path="/upshot" element={<UpshotPage />} />
+          <Route path="/us" element={<USPage />} />
+          <Route path="/world" element={<WorldPage />} />
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
@@ -39,5 +81,5 @@ export default function App() {
  </div>
   )
 }
-
+// arts, automobiles, books, business, fashion, food, health, home, insider, magazine, movies, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, t-magazine, travel, upshot, us, world
 
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,18 +6,33 @@ export default function Layout() {
     return (
         <div className={styles.layoutContainer}>
             <nav className={styles.newsPaperSections}>
-                <NavLink to="/topstories" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
-                Top Stories
-                </NavLink>
-                <NavLink to="/opinion" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
-                Opinion
-                </NavLink>
-                <NavLink to="/business" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
-                Business
-                </NavLink>        
-                <NavLink to="/food" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>
-                Food
-                </NavLink>
+                <NavLink to="/arts" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Arts</NavLink>
+                <NavLink to="/automobiles" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Automobiles</NavLink>
+                <NavLink to="/books" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Books</NavLink>
+                <NavLink to="/fashion" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Fashion</NavLink>        
+                <NavLink to="/food" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Food</NavLink>
+                <NavLink to="/health" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Health</NavLink>
+                {/* <NavLink to="/home" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Home</NavLink> */}
+                <NavLink to="/magazine" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Magazine</NavLink>
+                <NavLink to="/movies" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Movies</NavLink>
+                <NavLink to="/nyregion" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>NYRegion</NavLink>
+                <NavLink to="/obituaries" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Obituaries</NavLink>
+                <NavLink to="/opinion" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Opinion</NavLink>
+                <NavLink to="/politics" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Politics</NavLink>
+                <NavLink to="/realestate" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>RealEstate</NavLink>
+                <NavLink to="/science" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Science</NavLink>
+                <NavLink to="/sundayreview" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>SundayReview</NavLink>
+                <NavLink to="/technology" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Technology</NavLink>
+                <NavLink to="/topstories" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>TopStories</NavLink>
+                <NavLink to="/travel" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Travel</NavLink>
+                <NavLink to="/upshot" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Upshot</NavLink>
+                <NavLink to="/business" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Business</NavLink>   
+                <NavLink to="/sports" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>Sports</NavLink>
+                <NavLink to="/us" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>US</NavLink>
+                <NavLink to="/world" className={({isActive, isPending}) => isPending ? "pending" : isActive ? "active" : ""}>World</NavLink>
+
+
+
             </nav>
             <hr />
             <Outlet />
